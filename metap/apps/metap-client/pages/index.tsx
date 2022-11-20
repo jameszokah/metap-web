335ff54d0acaf7b6c0bd1601232a2d346aae9b30
James Zokah <jameszokah@gmail.com>
2022-11-20 15:40:46 +0000

    bootstraped nextjs with tailwindcss

---
+++ metap/apps/metap-client/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.css';
+
+export function Index() {
+
+  return (
+    <h1 className='text-4xl text-blue-300'>
+      <span> Hello there, </span>
+      Welcome metap-client 👋
+    </h1>
+  );
+}
+
+export default Index;
