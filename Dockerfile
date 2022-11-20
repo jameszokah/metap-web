e1d76ab699ef26efcf11657506e58189e29c051f
James Zokah <jameszokah@gmail.com>
2022-11-20 00:54:30 +0000

    added ssl key and cert

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ apt-get install -y build-essential pip net-tools iputils-ping iproute2 curl
 
 RUN curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && apt-get install -y nodejs
 WORKDIR /app
-EXPOSE 3001
-EXPOSE 2000-2020
+EXPOSE 3000-3000
+EXPOSE 3001-3001
+EXPOSE 3333-3333
+EXPOSE 5000-5050
 EXPOSE 10000-10100
--- metap/apps/metap-server/src/main.ts
+++ metap/apps/metap-server/src/main.ts
@@ -5,25 +5,26 @@
 
 import * as express from 'express';
 import * as path from 'path';
-import https from 'httpolyglot';
+import * as https from 'https';
 import * as fs from 'fs';
+
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 const option = {
-  key: fs.readFileSync('./ssl/key.pem', 'utf-8'),
-  cert: fs.readFileSync('./ssl/cert.pem', 'utf-8')
+  key: fs.readFileSync(path.resolve(__dirname, './assets/ssl/key.pem'), 'utf-8'),
+  cert: fs.readFileSync(path.resolve(__dirname, './assets/ssl/cert.pem'), 'utf-8')
 }
 
-const httpsServer = https.createServer(Option, app)
+const httpsServer = https.createServer(option, app);
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to metap-server!' });
 });
 
 const port = process.env.port || 3333;
-const server = app.listen(port, () => {
+const server = httpsServer.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
